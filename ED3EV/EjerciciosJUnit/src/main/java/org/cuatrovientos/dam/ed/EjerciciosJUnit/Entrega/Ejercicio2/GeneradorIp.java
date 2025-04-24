f8566c6c6ab1658d318f8e5d93e7dfe37a877d7c
Eliam68 <ian681968@gmail.com>
2025-04-24 12:24:27 +0200

    Se hizo la clase GeneradorIp

--- ED3EV/EjerciciosJUnit/src/main/java/org/cuatrovientos/dam/ed/EjerciciosJUnit/Entrega/Ejercicio2/GeneradorIp.java
+++ ED3EV/EjerciciosJUnit/src/main/java/org/cuatrovientos/dam/ed/EjerciciosJUnit/Entrega/Ejercicio2/GeneradorIp.java
@@ -1,5 +1,34 @@
 package org.cuatrovientos.dam.ed.EjerciciosJUnit.Entrega.Ejercicio2;
 
-public class GeneradorIp {
+import java.util.Random;
 
+public class GeneradorIp {
+	Random rnd = new Random();
+	private int parte1;
+	private int parte2;
+	private int parte3;
+	private int parte4;
+
+	private void generarNumero() {
+		parte1 = rnd.nextInt(255);
+		parte2 = rnd.nextInt(255);
+		parte3 = rnd.nextInt(255);
+		parte4 = rnd.nextInt(255);
+	}
+
+	private String generarIPV4() {
+		String resultado = "";
+		generarNumero();
+
+		if (parte1 == 0) {
+			parte1 += 1;
+		}
+		if (parte4 == 0) {
+			parte4 += 1;
+		}
+
+		resultado = parte1 + "." + parte2 + "." + parte3 + "." + parte4;
+		return resultado;
+		
+	}
 }
---
+++ ED3EV/EjerciciosJUnit/src/test/java/org/cuatrovientos/dam/ed/EjerciciosJUnit/Entrega/Ejercicio2/GeneradorIpTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.cuatrovientos.dam.ed.EjerciciosJUnit.Entrega.Ejercicio2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Se ejecuta el metodo 1000 veces para comprobar si el aleatorio cumple siempre con el objetivo
+ */
+class GeneradorIpTest {
+
+	@Test
+	void test() {
+		try {
+			
+			
+		}
+		catch{}
+	}
+
+}
